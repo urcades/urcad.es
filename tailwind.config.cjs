d7e722671f3625cdadd03a63df417a4afce9b089
é. urcades <edouard@urcad.es>
2023-05-31 10:26:57 -0400

    update: added custom night mode styling in config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			typography: (theme) => ({
+			  DEFAULT: {
+				css: {
+					'--tw-prose-body': theme('colors.black'),
+					'--tw-prose-invert-body': theme('colors.white'), 
+					'--tw-prose-invert-headings': theme('colors.white'),
+				},
+			  },
+			}),
+		  },
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
