cf7a53d2e2c23c902cf371652b0e4a36f980ac80
é. urcades <edouard@urcad.es>
2023-11-14 00:57:46 -0500

    fix: config export for site

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.urcad.es',
   integrations: [tailwind(), prefetch(), mdx()],
   prefetch: {
     prefetchAll: true
