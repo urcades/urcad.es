34a42622142d4c025a349be5017c45a29f8e468f
é. urcades <edouard@urcad.es>
2024-07-09 10:48:10 -0400

    update: rss filtering, packages

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,12 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
+
 const parser = new MarkdownIt();
 
 export async function GET(context) {
     const blog = await getCollection('writing');
+
     return rss({
         title: 'É. Urcades',
         description: 'Ongoing Writing',
@@ -17,9 +19,17 @@ export async function GET(context) {
             customData: post.data.customData,
             link: `/writing/${post.slug}/`,
             content: sanitizeHtml(parser.render(post.body), {
-                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-              }),
-              ...post.data,
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+                transformTags: {
+                    'Image': () => ({ tagName: '', text: '' }) // Remove Image components
+                },
+                exclusiveFilter: (frame) => {
+                    // Remove specific strings by filtering their content
+                    const importRegex = /import\s[^;]*;/g;
+                    return importRegex.test(frame.text);
+                }
+              }).replace(/import\s[^;]*;/g, ''), // Also replace instances of import statements in the content
+            ...post.data,
         })),
     });
 }
