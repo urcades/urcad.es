9e6b4bed2868e9643823a4113f537b3e13d4032c
é. urcades <edouard@urcad.es>
2024-10-23 09:30:20 -0400

    updating rss, index

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,23 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const blog = await getCollection('writing');
+  const blog = await getCollection("writing");
   return rss({
-    title: 'É. Urcades',
-    description: 'Ongoing Writing',
+    title: "É. Urcades",
+    description: "Ongoing Writing",
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       customData: post.data.customData,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
       // Compute RSS link from post `slug`
       // This example assumes all posts are rendered as `/writing/[slug]` routes
       link: `/writing/${post.slug}/`,
