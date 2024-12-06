c6ae42c358c2bf9076d1d3b18651694d261b0506
é. urcades <edouard@urcad.es>
2024-12-06 01:05:12 -0500

    update astro, small copy edits

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,24 @@
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 
+const drafts = defineCollection({});
+
 // Define a `type` and `schema` for each collection
 const writingCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      foregroundColor: z.string().optional(),
-      foregroundColorDark: z.string().optional(),
-      backgroundColor: z.string().optional(),
-      backgroundColorDark: z.string().optional(),
-    })
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    foregroundColor: z.string().optional(),
+    foregroundColorDark: z.string().optional(),
+    backgroundColor: z.string().optional(),
+    backgroundColorDark: z.string().optional(),
+  }),
 });
 
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   writing: writingCollection,
+  drafts, // Potentially fix this later with a 'real' implementation of drafts
 };
